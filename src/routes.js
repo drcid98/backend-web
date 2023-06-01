e866491a9600dd85c09845fb11ac20814eca2321
gvfigueroa <gvfigueroa@uc.cl>
2023-05-31 22:29:14 -0400

    sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const Router = require('koa-router');
-const characters = require('./routes/characters.js');
-const users = require('./routes/users.js');
+// const characters = require('./routes/characters.js');
+// const users = require('./routes/users.js');
 
 
 const router = new Router();
 
-router.use('/characters', characters.routes());
-router.use('/users', users.routes());
+// router.use('/characters', characters.routes());
+// router.use('/users', users.routes());
 
 module.exports = router;
