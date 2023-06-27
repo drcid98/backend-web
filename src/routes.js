0804158825e886024fc8a378c344f72d139c7bda
gvfigueroa <gvfigueroa@uc.cl>
2023-06-26 20:05:32 -0400

    manejo de sesion con jwt

--- src/routes.js
+++ src/routes.js
@@ -8,11 +8,16 @@ const fortify = require("./routes/fortify.js");
 const games = require("./routes/games.js");
 const players = require("./routes/players.js");
 const territories = require("./routes/territories.js");
+const authRoutes = require('./routes/authentication.js')
+const dotenv = require('dotenv');
+const jwtMiddleware = require('koa-jwt')
+// const scopeProtectedRoutes = require('./routes/scopeExample.js')
+
+dotenv.config();
 
 const router = new Router();
 
-// router.use('/characters', characters.routes());
-router.use("/users", users.routes());
+
 router.use("/start", start.routes());
 router.use("/draft", draft.routes());
 router.use("/attack", attack.routes());
@@ -20,5 +25,16 @@ router.use("/fortify", fortify.routes());
 router.use("/games", games.routes());
 router.use("/players", players.routes());
 router.use("/territories", territories.routes());
+
+router.use(authRoutes.routes());
+
+
+// Desde esta línea, todas las rutas requieriran un JWT. Esto no aplica para
+// las líneas anteriores
+router.use(jwtMiddleware( { secret: process.env.JWT_SECRET } ))
+
+router.use('/users', users.routes());
+
+// router.use('/scope-example', scopeProtectedRoutes.routes())
 
 module.exports = router;
