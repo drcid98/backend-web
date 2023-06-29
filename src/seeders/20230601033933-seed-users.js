3997b1189871c85c92a35bf0b9cdda2e033e743d
drcid89 <drcid98@gmail.com>
2023-06-29 01:08:57 -0400

    add seed

--- src/seeders/20230601033933-seed-users.js
+++ src/seeders/20230601033933-seed-users.js
@@ -14,6 +14,20 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date(),
     },
+    {
+      username:"qwerty",
+      password:"borito",
+      mail:"drcid2@uc.cl",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      username:"asdasd",
+      password:"borito",
+      mail:"drcid3@uc.cl",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
   ]),
   down: (queryInterface) => queryInterface.bulkDelete("Users", null, {}),
 };
