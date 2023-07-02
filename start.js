1e0969306dd441422fc0d448ee54999b1300aae1
Diego Rodriguez Cid <darodriguez6@uc.cl>
2023-07-02 00:15:30 -0400

    fix

--- start.js
+++ start.js
@@ -7,13 +7,21 @@ async function deploy() {
     console.log("Installing dependencies...");
     console.log(output1);
 
-    const {stdout: output2} = await exec("yarn db:migrate");
-    console.log("Migrating database...");
+    const { stdout: output2 } = await exec("yarn db:drop");
+    console.log("Dropping database...");
     console.log(output2);
 
-    const {stdout: output3} = await exec("yarn db:seed");
-    console.log("Seeding database...");
+    const { stdout: output3 } = await exec("yarn db:create");
+    console.log("Creating database...");
     console.log(output3);
+
+    const {stdout: output4} = await exec("yarn db:migrate");
+    console.log("Migrating database...");
+    console.log(output4);
+
+    const {stdout: output5} = await exec("yarn db:seed");
+    console.log("Seeding database...");
+    console.log(output5);
 }
 
 deploy();
